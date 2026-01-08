b12e85b5521cb76969776a7f5d4f4824146f9339
montyortiz78-dev <montyortiz78@gmail.com>
2026-01-07 21:12:41 -0600

    insert GTM code

--- storefront/src/app/layout.tsx
+++ storefront/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next"
+import Script from "next/script"
 import "styles/globals.css"
 import { ThemeProvider } from "lib/providers/theme-provider"
 import { KandiProvider } from "lib/context/kandi-context" // <--- Import Provider
@@ -13,7 +14,25 @@ export const metadata: Metadata = {
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      {/* 2. Paste the GTM "Head" Script here using the Script component */}
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-W2576T6Q');
+        `}
+      </Script>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W2576T6Q"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
